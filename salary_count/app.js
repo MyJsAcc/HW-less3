8f565e7b3e124a34b2b7e8698bde6bc982ed997f
MyJsAcc <snabavtoseo@gmail.com>
2021-03-30 13:18:15 +0300

    Добавил функцию рекурсию

--- salary_count/app.js
+++ salary_count/app.js
@@ -1,27 +1,70 @@
-// Задам переменны для моих ячеек под результаты
-
 const countSalaries = () => {
+  let countSalary = 0,
+    i = 0,
+    historySalary = "";
 
-let countSalary = 0, i = 0, historySalary = '' 
+  while (true) {
+    const salaryInput = +prompt("Введите сумму");
 
-while(true) {
-   
-    const salaryInput = +prompt('Введите сумму') 
- 
-    if(!isNaN(salaryInput) && salaryInput) { 
-        countSalary += salaryInput 
-        i++; 
-        historySalary +=  salaryInput + ' ' 
-        
-    } else { 
-       const isSomethingWrong = confirm('Хотите продолжить вычисление или получить результаты?') 
-       if(isSomethingWrong) {        
-       } else { // Иначе выводим результаты 
-        alert(`Сумма всех зарплат: ${countSalary} \n Средняя зарплата: ${countSalary/i} \n История ввода: ${historySalary}`)
-        break
-       }
+    if (!isNaN(salaryInput) && salaryInput) {
+      countSalary += salaryInput;
+      i++;
+      historySalary += salaryInput + " ";
+    } else {
+      const isSomethingWrong = confirm(
+        "Хотите продолжить вычисление или получить результаты?"
+      );
+      if (isSomethingWrong) {
+      } else {
+        // Иначе выводим результаты
+        alert(
+          `Сумма всех зарплат: ${countSalary} \n Средняя зарплата: ${
+            countSalary / i
+          } \n История ввода: ${historySalary}`
+        );
+        break;
+      }
     }
-}
+  }
+};
+// countSalaries();
 
-}
-countSalaries()
+// Через рекурсию
+
+const countAllSalaries = (
+  summ = 0,
+  count = 0,
+  averageSalary = 0,
+  history = ""
+) => {
+  const salaryQuestion = +prompt("Введите заработную плату сотрудника");
+  let salarySumm = summ;
+  if (salaryQuestion === 0 || isNaN(salaryQuestion)) {
+    let continueQuestion = confirm(
+      "Может хотите продолжить вычисления или получить ответ?"
+    );
+    if (!continueQuestion) {
+      return alert(`
+            Сумма зарплат: ${salarySumm} $
+            Средняя зарплата: ${averageSalary} $
+            История ввода: ${history}
+            `);
+    }
+    return countAllSalaries(
+      salarySumm + salaryQuestion,
+      count,
+      (averageSalary = salarySumm / count),
+      history
+    );
+  }
+  averageSalary = salarySumm / count;
+  history += salaryQuestion + " $";
+  count++;
+  return countAllSalaries(
+    salarySumm + salaryQuestion,
+    count,
+    averageSalary,
+    history
+  );
+};
+ countAllSalaries();
